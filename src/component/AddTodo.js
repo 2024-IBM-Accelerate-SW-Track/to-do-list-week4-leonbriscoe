3adad64ac52652bcd6f7f1d4c7c68a2378fdcd52
leondanquah <leondanq@buffalo.edu>
2024-07-08 23:15:48 -0400

    week4

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
 import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Axios from "axios";
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -39,14 +40,37 @@ class AddTodo extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
-      this.props.addTodo(this.state);
-      this.setState({
-        content: "",
-        date: "",
-        duedate: null
+      const jsonObject = {
+        id: Date.now(), 
+        task: this.state.content,
+        currentDate: this.state.date,
+        dueDate: this.state.duedate
+      };
+  
+      Axios({
+        method: "POST",
+        url: "http://localhost:8080/add/item", 
+        data: jsonObject,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(res => {
+        console.log(res.data.message);
+  
+        
+        this.props.addTodo(this.state);
+        this.setState({
+          content: "",
+          date: "",
+          duedate: null
+        });
+      }).catch(err => {
+        console.error("Error adding item: ", err);
       });
     }
   };
+  
+  
   render() {
     return (
       // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
